b219a815d7e6c0bbc5b26dc07cd9750e66235a06
Andreick <andreick.gomes.ismart@gmail.com>
2021-01-29 13:09:22 -0300

    Add javascript

---
+++ Js/main.js
@@ -0,0 +1,67 @@
+function clicou()
+{
+    document.getElementById("titulo").innerHTML = "<i>Você clicou</i>";
+    //alert("Você clicou");
+}
+
+function redirecionar()
+{
+    window.open("https://github.com/Andreick/javascript");
+    //window.location.href = "https://github.com/Andreick/javascript";
+}
+
+function trocar(texto)
+{
+    texto.innerHTML = "<b>Você passou o mouse</b>";
+    //document.getElementById("texto").innerHTML = "<b>Você passou o mouse</b>";
+    //alert("trocar texto");
+}
+
+function voltar(texto)
+{
+    texto.innerHTML = "Passe o mouse aqui";
+    //document.getElementById("texto").innerHTML = "Passe o mouse aqui";
+}
+
+function load()
+{
+    alert("Página carregada");
+}
+
+function mudar(select)
+{
+    console.log(select.value);
+}
+
+/*
+function soma(n1, n2)
+{
+    return n1 + n2;
+}
+
+//alert(soma(4,9));
+
+var date = new Date();
+alert(date.getDay());
+alert(date.getHours());
+alert(date.getMinutes());
+
+for (var i = 0; i < 5; i++)
+{
+    console.log(i);
+}
+
+//var idade = prompt("Qual a sua idade?");
+
+//var frutas = [{nome:"maçã", cor:"vermelho"}, {nome:"uva", cor:"roxo"}];
+//console.log(frutas[1].cor);
+
+//var fruta = {nome:"maçã", cor:"vermelho"};
+//console.log(fruta.cor);
+
+//var lista = ["maçâ", "pêra", "laranja"];
+//lista.push("uva");
+//lista.pop();
+//console.log(lista.reverse());
+//console.log(lista.toString());
+*/
