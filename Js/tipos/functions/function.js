1f6114603acce21095a8ff2c5be0316333fead44
Andreick <andreick.gomes.ismart@gmail.com>
2021-01-30 12:42:26 -0300

    Reestrutura pasta

---
+++ Js/tipos/functions/function.js
@@ -0,0 +1,50 @@
+function fn()
+{
+    return "Code here";
+}
+
+// Equivalente à declaração acima
+const arrowFn = () => "Code here";
+const arrowFn2 = () => {
+    // Mais de uma expressão
+    return "Code here";
+}
+
+// Funções também são objetos
+fn.prop = "Posso criar propriedades";
+
+console.log(fn());
+console.log(fn.prop);
+
+// Receber parâmetros
+const logValue = value => console.log(value);
+const logFnResult = fnParam => console.log(fnParam());
+
+logValue("Value here");
+logFnResult(fn);
+
+// Receber e retornar funções
+const controlFnExec => fnParam => allowed => {
+
+    if (allowed)
+    {
+        fnParam();
+    }
+}
+
+const handleFnExecution = controlFnExec(fn);
+
+handleFnExecution(true); // Exucutará a função fn
+handleFnExecution(); // Não executará a função fn
+
+// controlFnExec como função
+function controlFnExec(fnParam)
+{
+    return function(allowed)
+    {
+        if (allowed)
+        {
+            fnParam();
+        }
+    }
+}
