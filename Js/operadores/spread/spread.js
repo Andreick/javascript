57102685d0b6a9bd2c4566c92ddb2a9256713725
Andreick <andreick.gomes.ismart@gmail.com>
2021-01-30 21:59:23 -0300

    Add rest operator em spread.js

--- Js/operadores/spread/spread.js
+++ Js/operadores/spread/spread.js
@@ -1,4 +1,4 @@
-// Spread ...
+// Spread operator ...
 var partes = ["ombro", "joelhos"];
 var musica = ["cabeça", ...partes, "e", "pés"];
 
@@ -11,3 +11,10 @@ fn(...args);
 
 // Equivalente à
 fn(0, 1, 2);
+
+// Rest operator ...
+
+// Transforma os argumentos em um array
+function restFn(...args) { console.log(args) }
+
+restFn(3, 7, 8, 3, 0, 1, 3); // n parâmetros possíveis
