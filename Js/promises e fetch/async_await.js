0b1b4a7c32f7c5d82e112cb0e91d11e4b14980b8
Andreick <andreick.gomes.ismart@gmail.com>
2021-01-31 13:37:09 -0300

    Add EventEmitter em async_await.js

--- Js/promises e fetch/async_await.js
+++ Js/promises e fetch/async_await.js
@@ -17,3 +17,32 @@ simpleFunc().then(data => {
 }).catch(err => {
     console.log(err)
 });
+
+// EventEmitter
+
+const EventEmitter = require("events");
+
+const emitter = new EventEmitter();
+
+emitter.on("User logged", data => {
+    console.log(data);
+});
+
+emitter.emit("User logged", { user: "Roberto James" });
+
+class Users extends EventEmitter {
+    userLogged(data) {
+        setTimeout(() => {
+            this.emit("User logged", data);
+        }, 2000);
+    }
+}
+
+const users = new Users();
+
+users.once("User logged", data => { // Once executa apenas o primeiro evento
+    console.log(data);
+});
+
+users.userLogged({ user: "Roberto James" });
+users.userLogged({ user: "Valente Marques" });
